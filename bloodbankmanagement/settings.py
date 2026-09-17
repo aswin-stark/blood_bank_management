f4845e7a02d2298478f5e0028bb2d9694bf57b5e
ASWIN S <128965456+aswin-stark@users.noreply.github.com>
2026-09-17 23:30:44 +0530

    Update settings.py

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -23,11 +23,10 @@ MEDIA_URL='/media/'
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '+zy!9k=9pql5gz9bkqjore)k6r!%w0atk(@(!(!zvp5e(t2i8n'
-SECRET_KEY = 'rtd455rewef@#$%yutrxcvbnuiytrxcfvbt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     "blood-bank-management-9xv8.onrender.com",
