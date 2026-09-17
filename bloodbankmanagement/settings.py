2c216df4075bbd07e1295b8c6fedbb63651efe26
ASWIN S <128965456+aswin-stark@users.noreply.github.com>
2026-09-17 21:42:04 +0530

    Update ALLOWED_HOSTS for production settings

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = 'rtd455rewef@#$%yutrxcvbnuiytrxcfvbt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "blood-bank-management-9xv8.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
